f1c627f28954008a4ed06f0e1eb44ac0d9a8b415
VolodymyrKirichenko <109915550+VolodymyrKirichenko@users.noreply.github.com>
2022-10-18 15:51:38 +0300

    solution (#1)

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,76 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export enum SortOrder {
+  Asc = 'asc',
+  Desc = 'desc',
+}
 
+function averageScore(students: Student): number {
+  return students.grades.reduce(
+    (acc: number, current: number) => acc + current, 0,
+  ) / students.grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const isAsc: boolean = order === SortOrder.Asc;
+  const array = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      array.sort((studentOne, studentTwo) => {
+        return isAsc
+          ? studentOne[sortBy].localeCompare(studentTwo[sortBy])
+          : studentTwo[sortBy].localeCompare(studentOne[sortBy]);
+      });
+      break;
+
+    case SortType.Age:
+      array.sort((studentOne, studentTwo) => {
+        return isAsc
+          ? studentOne.age - studentTwo.age
+          : studentTwo.age - studentOne.age;
+      });
+      break;
+
+    case SortType.Married:
+      array.sort((studentOne, studentTwo) => {
+        return isAsc
+          ? Number(studentOne.married) - Number(studentTwo.married)
+          : Number(studentTwo.married) - Number(studentOne.married);
+      });
+      break;
+
+    case SortType.AverageGrade:
+      array.sort((studentOne, studentTwo) => {
+        return isAsc
+          ? averageScore(studentOne) - averageScore(studentTwo)
+          : averageScore(studentTwo) - averageScore(studentOne);
+      });
+      break;
+
+    default:
+      throw new Error('there is no mistake');
+  }
+
+  return array;
 }
